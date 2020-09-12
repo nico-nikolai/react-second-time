7b2f026f353c7a505324a06093822e896900cd23
Nick Rose <nickfbp@gmail.com>
2020-09-12 12:03:31 -0600

    Fixed an Error-Workshop Week 2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardTitle, CardBody} from 'reactstrap';
+import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
 
 class CampsiteInfo extends Component {
 
@@ -7,11 +7,11 @@ class CampsiteInfo extends Component {
     renderCampsite(campsite) {
         return (
             <div className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardBody>
                             <CardTitle>{campsite.name}</CardTitle>
-                            {campsite.description}
+                            <CardText>{campsite.description}</CardText>
                         </CardBody>
                     </Card>
             </div>
