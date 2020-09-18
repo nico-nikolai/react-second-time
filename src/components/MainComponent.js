90a918030a4b3fedc84f8a0c6c1b460f67a3d4a2
Nick Rose <nickfbp@gmail.com>
2020-09-18 11:21:32 -0600

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-//import CampsiteInfo from './CampsiteInfoComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -33,12 +33,22 @@ class Main extends Component {
       )
     }
 
+    const CampsiteWithId = ({match}) => {
+      return (
+        <CampsiteInfo
+          campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+          comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+        />
+      )
+    }
+
       return (
           <div>
             <Header />
             <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/directory' render={() => <Directory campsites = {this.state.campsites} />} />
+              <Route path='/directory/:campsiteId' component={CampsiteWithId} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to='/home' />
             </Switch>
